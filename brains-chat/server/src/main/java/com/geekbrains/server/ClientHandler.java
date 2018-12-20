29e57dc890606576033467a21c8309842180420a
Vladimir <maz0bk@gmail.com>
2018-12-20 23:50:07 +0300

    добавил чат

---
+++ brains-chat/server/src/main/java/com/geekbrains/server/ClientHandler.java
@@ -0,0 +1,85 @@
+package com.geekbrains.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private String nickname;
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String msg = in.readUTF();
+                        // /auth login1 pass1
+                        if (msg.startsWith("/auth ")) {
+                            String[] tokens = msg.split("\\s");
+                            String nick = server.getAuthService().getNicknameByLoginAndPassword(tokens[1], tokens[2]);
+                            if (nick != null && !server.isNickBusy(nick)) {
+                                sendMsg("/authok " + nick);
+                                server.subscribe(this);
+                                nickname = nick;
+                                break;
+                            }
+                        }
+                    }
+                    while (true) {
+                        String msg = in.readUTF();
+                        if (msg.equals("/end")) {
+                            break;
+                        }
+                        server.broadcastMsg(nickname + ": " + msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    ClientHandler.this.disconnect();
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        server.unsubscribe(this);
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ brains-chat/server/src/main/java/com/geekbrains/server/Server.java
@@ -0,0 +1,56 @@
+package com.geekbrains.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+    private AuthService authService;
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен на порту 8189");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ClientHandler(this, socket);
+                System.out.println("Подключился новый клиент");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Сервер завершил свою работу");
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o : clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+    public boolean isNickBusy(String nickname) {
+        for (ClientHandler o : clients) {
+            if (o.getNickname().equals(nickname)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
