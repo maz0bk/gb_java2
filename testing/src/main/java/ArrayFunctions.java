b40a3c5e41832ebcae8c4e32e35a562de183cd0b
Vladimir <maz0bk@gmail.com>
2019-01-31 15:56:53 +0300

    дз

---
+++ testing/src/main/java/ArrayFunctions.java
@@ -0,0 +1,11 @@
+public class ArrayFunctions {
+    public static int [] endAfterFour(int mas[]) throws RuntimeException{
+        if (mas.length == 0) throw new RuntimeException();
+
+        for (int i = mas.length-1; i >=0 ; i--) {
+            if (mas[i]==4)
+
+        }
+        return
+    }
+}
